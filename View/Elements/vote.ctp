b597d0587e65fb85876b02c72554f9df710b9a12
Alfonso Etxeberria <alfonso@Arambi.local>
2014-02-24 13:39:00 +0400

    Modificado el javascript de vote

--- View/Elements/vote.ctp
+++ View/Elements/vote.ctp
@@ -48,30 +48,3 @@
 		
 	</div>
 </div>
-
-<?/* if( !$user_vote): ?>
-
-    <?= $this->Html->link( __d( 'vote', "A favor"), array(
-        'plugin' => 'vote',
-        'controller' => 'votes', 
-        'action' => 'add',
-        $model,
-        $foreign_key,
-        'positive',
-        'ext' => 'json',
-    ), array(
-        'class' => 'cake-vote'
-    )) ?>
-  
-    <?= $this->Html->link( __d( 'vote', "En contra"), array(
-        'plugin' => 'vote',
-        'controller' => 'votes', 
-        'action' => 'add',
-        $model,
-        $foreign_key,
-        'negative',
-        'ext' => 'json',
-    ), array(
-        'class' => 'cake-vote'
-    )) ?>
-  <? endif */?>
--- webroot/js/vote.jquery.js
+++ webroot/js/vote.jquery.js
@@ -1,16 +1,20 @@
 $(function(){
-  $(".cake-votes a").click( function(){
+  $(".vote-box a").click( function(){
     var _this = $(this);
     $.ajax({
       url: _this.attr( 'href'),
+      beforeSend: function(){
+        $(".vote-box").fadeTo( 100, .3);
+      },
       success: function( data){
-        var div = _this.parent( '.cake-votes');
+        var div = _this.parents( '.vote-box');
         $(div).find( '.vote-positive').html( data.positives);
         $(div).find( '.vote-negative').html( data.negatives);
         $(div).find( 'a').each( function(){
 		      var el = $(this);
-		      $(this).replaceWith( '<span>' + el.html() + '</span>');
+		      $(this).replaceWith( '<span class="vote-icon vote-inactive">' + el.html() + '</span>');
         });
+        $(".vote-box").fadeTo( 100, 1);
       }
     })
     return false;
